a8d773630d44aa303d8ab0dce5a0925c19ab9f20
Mariam <Mariamhh98@hotmail.com>
2019-09-08 09:35:18 +0300

    Done!

--- booking/urls.py
+++ booking/urls.py
@@ -1,22 +1,10 @@
-"""booking URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+ 
 from django.contrib import admin
 from django.urls import path
 
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 from hotels import views
 
@@ -24,15 +12,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('hotels/list/', views.HotelsList.as_view(), name="hotels-list"),
-    path('hotels/details/<int:hotel_id>/', views.HotelDetails, name="hotel-details"),
+    path('hotels/details/<int:hotel_id>/', views.HotelDetails.as_view(), name="hotel-details"),
     path('hotels/book/<int:hotel_id>/', views.BookHotel.as_view(), name="book-hotel"),
 
     path('bookings/', views.BookingsList.as_view(), name="bookings-list"),
     path('bookings/cancel/<int:booking_id>/', views.CancelBooking.as_view(), name="cancel-booking"),
     path('bookings/modify/<int:booking_id>/', views.ModifyBooking.as_view(), name="modify-booking"),
 
-    path('profile/', Profile.as_view(), name="profile"),
+    path('profile/', views.Profile.as_view(), name="profile"),
 
-    path('login/', obtain_jwt_token, name="login"),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
     path('register/',  views.Register.as_view() , name="register"),
 ]
